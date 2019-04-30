cf08cfeab64510880a13fd751718e2430184b2e6
benyou1969 <ismail.mca191@gmail.com>
2019-04-30 14:02:20 +0100

    Key modifiers

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="hello">
     <div>
-      <input type="text" v-model="name">
-       Type your name to show it: <b>{{ name }}</b>
+      <button @click="clickEvent">click me</button>
+      <button @click.exact="clickExactEvent">click me</button>
+      <button @click.shift="shiftEvent">shift me</button>
+      <button @click.alt="altEvent">alt me</button>
+      <button @click.once="price++">{{ price }}</button>
+      <button @click.meta="preventEvent">Stop Google</button>
+      <a @click.prevent="preventEvent" href="http://www.google.com">Google</a>
     </div> 
   </div>
 </template>
@@ -12,11 +17,26 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
+      price:10,
       name: ''
     }
   },
   methods: {
-    
+    clickEvent(){
+      console.log('clicked...')
+    },
+    clickExactEvent(){
+      console.log('you pressed only click event...')
+    },
+    shiftEvent(){
+      console.log('shifted...')
+    },
+    altEvent(){
+      console.log('alted...')
+    },
+    preventEvent(){
+      console.log('prevent')
+    }
   },
 }
 </script>
