3f5f70673573705e0f1ef1a6645dba3650d7fe82
benyou1969 <ismail.mca191@gmail.com>
2019-04-30 13:10:54 +0100

    event object + mouse move

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="hello"> 
-    <div class="divGrey" @mousemove="logEvent">
-      <b>X: {{ coords.offsetX }}</b>,
-      <b>Y: {{ coords.offsetY }}</b>
+    <input type="text" @blur="updateName">
+    <div class="divGrey" @mouseup="sendAlert(e)">
+            {{ name }}
     </div>
   </div>
 </template>
@@ -12,111 +12,27 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      coords:{
-      offsetX: 0,
-      offsetY: 0,
-      }
+      name: ''
     }
   },
   methods: {
-    logEvent(e){
-       this.coords.offsetX = e.offsetX
-       this.coords.offsetY = e.offsetY
+    updateName(e) {
+      return this.name = e.target.value
+    },
+    sendAlert(e){
+        console.log(e)
+        console.log(e.srcElement.baseURI)
+        alert("mouse moved up" )
      }
   },
 }
 </script>
 
 <style scoped>
-/* 
-  ##Device = Desktops
-  ##Screen = 1281px to higher resolution desktops
-*/
-
-@media (min-width: 1281px) {
-  
-  
-}
-
-/* 
-  ##Device = Laptops, Desktops
-  ##Screen = B/w 1025px to 1280px
-*/
-
-@media (min-width: 1025px) and (max-width: 1280px) {
-  .divGrey{
-  width: 500px;
-  height: 150px;
-  background-color: grey;
-  border-radius: 5px;
-  margin-left: 30%;
-  color: white;
-  }  
-}
-
-/* 
-  ##Device = Tablets, Ipads (portrait)
-  ##Screen = B/w 768px to 1024px
-*/
-
-@media (min-width: 768px) and (max-width: 1024px) {
-    .divGrey{
-    width: 500px;
-    height: 150px;
-    background-color: grey;
-    border-radius: 5px;
-    margin-left: 22%;
-    color: white;
-    }
-}
-
-/* 
-  ##Device = Tablets, Ipads (landscape)
-  ##Screen = B/w 768px to 1024px
-*/
-
-/* @media (min-width: 768px) and (max-width: 1024px) and (orientation: landscape) {
-    .divGrey{
-    width: 500px;
-    height: 150px;
-    border: 1px solid #41b883;
-    background-color: grey;
-    border-radius: 5px;
-    margin-left: 22%;
-    }
-  
-} */
-
-/* 
-  ##Device = Low Resolution Tablets, Mobiles (Landscape)
-  ##Screen = B/w 481px to 767px
-*/
-
-@media (min-width: 481px) and (max-width: 767px) {
-    .divGrey{
-    width: 400px;
-    height: 150px;
-    background-color: grey;
-    border-radius: 5px;
-    margin-left: 12%;
-    color: white;
-    }
-  
-}
-
-/* 
-  ##Device = Most of the Smartphones Mobiles (Portrait)
-  ##Screen = B/w 320px to 479px
-*/
-
-@media (min-width: 320px) and (max-width: 480px) {
-    .divGrey{
-    width: 300px;
-    height: 150px;
-    background-color: grey;
-    border-radius: 5px;
-    margin-left: 8%;
-    color: white;
-    }
+.divGrey{
+width: 300px;
+height: 100px;
+color: white;
+background: grey;
 }
 </style>
