abe96b8fba9d0f39db4aa12a93b280e94a11b29f
benyou1969 <ismail.mca191@gmail.com>
2019-04-30 14:17:09 +0100

    cleaning the component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,7 @@
 <template>
   <div class="hello">
     <div>
-      <button @click="clickEvent">click me</button>
-      <button @click.exact="clickExactEvent">click me</button>
-      <button @click.shift="shiftEvent">shift me</button>
-      <button @click.alt="altEvent">alt me</button>
-      <button @click.once="price++">{{ price }}</button>
-      <button @click.meta="preventEvent">Stop Google</button>
-      <a @click.prevent="preventEvent" href="http://www.google.com">Google</a>
+      
     </div> 
   </div>
 </template>
@@ -17,26 +11,11 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      price:10,
-      name: ''
+      
     }
   },
   methods: {
-    clickEvent(){
-      console.log('clicked...')
-    },
-    clickExactEvent(){
-      console.log('you pressed only click event...')
-    },
-    shiftEvent(){
-      console.log('shifted...')
-    },
-    altEvent(){
-      console.log('alted...')
-    },
-    preventEvent(){
-      console.log('prevent')
-    }
+    
   },
 }
 </script>
