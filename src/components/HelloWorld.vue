6bd01497b4cca40f4bc4d9f2f658d228768a2dd5
benyou1969 <ismail.mca191@gmail.com>
2019-04-30 13:25:07 +0100

    v-model

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,9 @@
 <template>
   <div class="hello">
     <div>
-      <input type="text" @keyup="updateName">
+      <input type="text" v-model="name">
        Type your name to show it: <b>{{ name }}</b>
     </div> 
-    <div>
-      <input type="text" @keypress="updateName">
-       Type your name and Keypress to show it: <b>{{ name }}</b>
-    </div> 
-    <div>
-      <input type="text" @keyup.enter="updateName">
-       Keyup and Press enter to show the name: <b>{{ name }}</b>
-    </div> 
-    
   </div>
 </template>
 
@@ -25,9 +16,7 @@ export default {
     }
   },
   methods: {
-    updateName(e) {
-      return this.name = e.target.value
-    }
+    
   },
 }
 </script>
