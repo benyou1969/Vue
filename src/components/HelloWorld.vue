cc62623eceed89446a6b4c4efe493d391f27e940
benyou1969 <ismail.mca191@gmail.com>
2019-05-01 13:24:06 +0100

    Vue cli

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,25 +1,12 @@
 <template>
   <div class="hello">
-    <div>
-      <h2>Color: Basic v-for</h2>
-      <!-- The index is the position of the item in the array -->
-      <ul>
-        <li v-for="(color, index) in colors">{{ index }}. {{ color }}</li>
-      </ul>
-      <hr>
-      <h2>Users: Loop objects (Multi arrays)</h2>
-      <ul>
-        <li v-for="user in users" :key="user.id">{{ user.name +' => '+ user.gender }}</li>
-      </ul>
-      <p>Only actives user</p>
-      <ul>
-        <li
-          v-for="user in users"
-          :key="user.id"
-          v-if="user.active"
-        >{{ user.name +' => '+ user.gender }}</li>
-      </ul>
-    </div>
+   <p>Vue cli stand for: Command Line Interface</p>
+   It's good to use vue to control parts of websites
+   A better workflow with WebPack
+   use ES6 that are not already supported 
+   compile and mninify code into one file
+   use single-file vue templates
+   use a live reload development server
   </div>
 </template>
 
@@ -28,23 +15,14 @@ export default {
   name: "HelloWorld",
   data() {
     return {
-      colors: ["red", "white", "black"], // string
-      users: [
-        { id: 1, name: "Benyou", gender: "male", active: true }, // Object
-        { id: 2, name: "Hannah", gender: "female", active: false },
-        { id: 3, name: "Sarah", gender: "female", active: true },
-        { id: 4, name: "Mark", gender: "male", active: false }
-      ]
+    
     };
   },
-  methods: {}
+  methods: {
+
+  }
 };
 </script>
 
 <style scoped>
-li {
-  list-style: none;
-  font-style: inherit;
-  font-size: 1.3rem;
-}
 </style>
