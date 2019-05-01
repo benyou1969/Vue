0551a230c43450d5b3dd8131b6292b37767d3453
benyou1969 <ismail.mca191@gmail.com>
2019-05-01 11:43:47 +0100

    looping with v-for

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,34 +1,50 @@
 <template>
   <div class="hello">
     <div>
-      <p v-if="shortName">Benyou</p>
-      <p v-else-if="toggleAge">15</p>
-      <button @click="toggleClick">showName is {{ shortName }}</button>
-      <button @click="toggleAge">showAge is {{ age }}</button>
-    </div> 
+      <h2>Color: Basic v-for</h2>
+      <!-- The index is the position of the item in the array -->
+      <ul>
+        <li v-for="(color, index) in colors">{{ index }}. {{ color }}</li>
+      </ul>
+      <hr>
+      <h2>Users: Loop objects (Multi arrays)</h2>
+      <ul>
+        <li v-for="user in users" :key="user.id">{{ user.name +' => '+ user.gender }}</li>
+      </ul>
+      <p>Only actives user</p>
+      <ul>
+        <li
+          v-for="user in users"
+          :key="user.id"
+          v-if="user.active"
+        >{{ user.name +' => '+ user.gender }}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  data() {
     return {
-      shortName: false,
-      age: true
-    }
+      colors: ["red", "white", "black"], // string
+      users: [
+        { id: 1, name: "Benyou", gender: "male", active: true }, // Object
+        { id: 2, name: "Hannah", gender: "female", active: false },
+        { id: 3, name: "Sarah", gender: "female", active: true },
+        { id: 4, name: "Mark", gender: "male", active: false }
+      ]
+    };
   },
-  methods: {
-    toggleClick(){
-      this.shortName = !this.shortName
-    },
-    toggleAge() {
-      this.age = !this.age
-    }
-  },
-}
+  methods: {}
+};
 </script>
 
 <style scoped>
-
+li {
+  list-style: none;
+  font-style: inherit;
+  font-size: 1.3rem;
+}
 </style>
