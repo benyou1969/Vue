f81674f20defa1066cdec187cdfc4e743001747e
benyou1969 <ismail.mca191@gmail.com>
2019-05-01 08:35:35 +0100

    hello

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="hello">
     <div>
-      
+      <p v-if="shortName">Benyou</p>
+      <p v-else-if="toggleAge">15</p>
+      <button @click="toggleClick">showName is {{ shortName }}</button>
+      <button @click="toggleAge">showAge is {{ age }}</button>
     </div> 
   </div>
 </template>
@@ -11,11 +14,26 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      
+      shortName: false,
+      age: true
     }
   },
   methods: {
-    
+    toggleClick(){
+      // const short = this.shortName
+      // if(short== true) {
+
+      // if(this.shortName == true) {
+      //   this.shortName = false
+      // }else{
+      //   this.shortName = true
+      // }
+  
+      this.shortName = !this.shortName
+    },
+    toggleAge() {
+      this.age = !this.age
+    }
   },
 }
 </script>
