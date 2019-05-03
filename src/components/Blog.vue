83d4279429379de508e37e943e146f7232734b60
benyou1969 <ismail.mca191@gmail.com>
2019-05-03 12:30:12 +0100

    Filter reverse string by using for loop

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -1,7 +1,7 @@
 <template>
       <div>
-           <p> {{ lorum | FirstLetterUpperCase  | Backwards}}</p>
-           <p>{{ word | Backwards }}</p>
+           <!-- <p> {{ lorum | FirstLetterUpperCase  | Backwards}}</p> -->
+           <p>{{ word | reverseString }}</p>
       </div>
 </template>
 
@@ -17,15 +17,22 @@ export default {
       },
       filters: {
             // 'Locally'
-            FirstLetterUpperCase: function(value) {
-                 if(!value) return ''
-                 value = value.toString();
-                 return value.charAt(0).toUpperCase() + value.slice(1)
-           },
-        
-           Backwards: function(str) {
-                 return str.split("").reverse().join("")
-           }
+            // 2. Reverse a String With a Decrementing For Loop
+            // link: https://medium.freecodecamp.org/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb
+            reverseString: function(str) {
+                  // Step one: create an empty string that will host the new created string
+                  var newString = ""
+                  // Step two: create a new for loop
+                  /* The starting point of the loop will be (str.length - 1) which corresponds to the 
+                        last character of the string, "o"
+                        As long as it is greater than or equals 0, the loop will go on
+                        We decrement i after each iteration */
+                  for (var i=str.length -1; i >=0; i--){
+                        newString += str[i]
+                  }
+                  return newString; 
+
+            }
       },
       methods: {
            
