7656a781cdf5b8ac29d05b3b79e7e0b0e843ce30
benyou1969 <ismail.mca191@gmail.com>
2019-05-03 13:13:45 +0100

    Reverse an Array

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -1,8 +1,10 @@
 <template>
-      <div>
-           <!-- <p> {{ lorum | FirstLetterUpperCase  | Backwards}}</p> -->
-           <p>{{ word | reverseString }}</p>
-      </div>
+  <div>
+    <p>{{ word }}</p>
+    <ul> 
+          <li v-for="(item,index) in items" :key="index" @click="reverseArr">{{ item }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -12,30 +14,18 @@ export default {
       data(){
             return{
                   word: 'food',
-                  lorum: 'lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad, dolor fuga sit adipisci vero quisquam perferendis qui nihil nesciunt fugiat quis rerum animi eos ex praesentium eligendi cum sint neque.'
+                  items: [
+                        23,32,54,76,87
+                  ]
             }
       },
       filters: {
-            // 'Locally'
-            // 2. Reverse a String With a Decrementing For Loop
-            // link: https://medium.freecodecamp.org/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb
-            reverseString: function(str) {
-                  // Step one: create an empty string that will host the new created string
-                  var newString = ""
-                  // Step two: create a new for loop
-                  /* The starting point of the loop will be (str.length - 1) which corresponds to the 
-                        last character of the string, "o"
-                        As long as it is greater than or equals 0, the loop will go on
-                        We decrement i after each iteration */
-                  for (var i=str.length -1; i >=0; i--){
-                        newString += str[i]
-                  }
-                  return newString; 
-
-            }
+            
       },
       methods: {
-           
+            reverseArr() {
+                   return this.items = this.items.reverse();
+            }
       },
 }
 </script>
