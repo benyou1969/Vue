96e470edf5d89b0ae22755d67d1f4bdaca71262c
benyou1969 <ismail.mca191@gmail.com>
2019-05-03 11:50:54 +0100

    Filter locally: Backwards and Uppercase

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -1,33 +1,43 @@
 <template>
       <div>
-           <ul>
-                 <li v-for="item in items" :key="item.id"> {{ item.title | snippet }}</li>
-           </ul>
+           <p> {{ lorum | FirstLetterUpperCase }}</p>
+           <p>{{ word | Backwards }}</p>
       </div>
 </template>
 
 <script>
-import axios from 'axios'
 
 export default {
       name: 'blog',
       data(){
             return{
-                  items: []
+                  word: 'food',
+                  lorum: 'lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad, dolor fuga sit adipisci vero quisquam perferendis qui nihil nesciunt fugiat quis rerum animi eos ex praesentium eligendi cum sint neque.'
             }
+      },
+      filters: {
+            // 'Locally'
+            FirstLetterUpperCase: function(value) {
+                 if(!value) return ''
+                 value = value.toString();
+                 return value.charAt(0).toUpperCase() + value.slice(1)
+           },
+           Backwards: function (value) {
+            //     there are Four method to reverse a sting (link: https://medium.freecodecamp.org/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb)
+                  if(!value) return ''
+                  value = value.toString()
+            //    step one: use split method to turn a new array
+                  value = value.split("")
+            //    step two: Use the reverse() method to reverse the new array
+                  value = value.reverse(value)
+            //     step three: Use Join to Joing all Elements of array into string
+                  value = value.join("");
+            //    Step four Return the reversed string
+                  return value
+           }
       },
       methods: {
            
       },
-     created(){
-           axios.get('https://jsonplaceholder.typicode.com/posts')
-            .then(response => {
-                 console.log(response.data)
-                 this.items = response.data
-           })
-           .catch(error => {
-                 console.log(error)
-           })
-     }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -3,18 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 
-// if we want to create a filter globaly it means u will create it in main.js where vue kicks in and take control of our application
-
-// function (val) => {
-  
-// }
-Vue.filter('snippet', val => {
-  // !val (if this doesn't exists)
-  if(!val || typeof(val) != 'string') return ''
-  val = val.slice(0,20)
-  return val
-} )
-
+Vue.filter('backwards', value => {
+ if ( !value) return ''
+  return value.charAt(0).toUpperCase() + value.slice(1)
+});
 
 Vue.config.productionTip = false
 
