d75971c00dc74497dbca67079715a3bfc2cbbf6f
benyou1969 <ismail.mca191@gmail.com>
2019-05-04 13:20:00 +0100

    Route parameters params

---
+++ src/components/Profile.vue
@@ -0,0 +1,17 @@
+<template>
+      <div class="view-profile">
+            <h2>Profile</h2>
+            {{ userID }}
+      </div>
+</template>
+
+<script>
+export default {
+      name: 'Profile',
+      data(){
+            return {
+                  userID: this.$route.params.user_id
+            }
+      }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Router from 'vue-router'
 import Home from '@/components/Home'
 import About from '@/components/About'
 import Contact from '@/components/Contact'
+import Profile from '@/components/Profile'
 
 
 Vue.use(Router)
@@ -24,6 +25,11 @@ export default new Router({
                   path: '/contact',
                   name: 'Contact',
                   component: Contact
+            },
+            {
+                  path: '/users/:user_id',
+                  name: 'Profile',
+                  component: Profile
             }
       ]
 })
