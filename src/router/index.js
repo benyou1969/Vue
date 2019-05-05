0e9f736f6834f12a36ae4ed955311bf8a91e002c
benyou1969 <ismail.mca191@gmail.com>
2019-05-05 12:25:41 +0100

    hash vs history mode

--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,14 @@ import Profile from '@/components/Profile'
 
 Vue.use(Router)
 
+// Why the symbol # is there between the '/#/' 
+// if we go to a website and type '/home' what's happening? we making a request to the serve and the server looked the request and said okay i'll need to send a home page to the browser...
+// but when we using vuejs in the front-end we don't want to be sending these requests to the serve all the time we need vuejs to take ccare all the work and when it sees the '/#/home' it prevents that request because this does not send request to the serve then vue js looked this url and says  i'll handle this route and i'll send you to home or about component,and that's why the hash symbol is there
+
+//  but it still not awesome to publish that in production mode  so how we remove it
+
 export default new Router({
+      mode : 'history',
       routes: [
             {
                   path: '/',
