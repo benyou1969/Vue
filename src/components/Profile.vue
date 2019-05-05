1b7a1197b77c6e88790f3d82a9eb333c5153944e
benyou1969 <ismail.mca191@gmail.com>
2019-05-05 11:30:44 +0100

    watching the route object

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,7 +1,7 @@
 <template>
       <div class="view-profile">
             <h2>Profile</h2>
-            {{ userID }}
+            {{ userId }}
       </div>
 </template>
 
@@ -10,8 +10,22 @@ export default {
       name: 'Profile',
       data(){
             return {
-                  userID: this.$route.params.user_id
+                  userId: this.$route.params.user_id
             }
+      },
+      methods:{
+            updateId() {
+              this.userId = this.$route.params.user_id   
+            }
+      },
+      watch: {
+            // I did in last commit when we went from one profile to another it changed this.$route parameter but unfortunately because vue js is recilying the component and it doesn't distroy it
+
+            // the soultion for that is by creating a watch that will be watch this property route and when it change we can react by updating the userId
+
+            // first step is create "watch" property and it's an object to watch certain things such as variables and object and when that variable or object changed then we can called a function to react
+
+            $route: 'updateId'
       }
 }
 </script>
