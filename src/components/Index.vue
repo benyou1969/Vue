7de45d64824d9c305c59f13b0c322a3f5754bbcb
benyou1969 <ismail.mca191@gmail.com>
2019-05-06 22:43:41 +0100

    add smoothies and display them in index page

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,6 +1,13 @@
 <template>
-      <div>
-            <h3>All Smoothies</h3>
+      <div class="index container">
+            <div class="card" v-for="smoothie in smoothies" :key="smoothie.id">
+                  <div class="card-content">
+                        <h2 class="purple-text">{{ smoothie.title }}</h2>
+                        <ul class="ingredients">
+                              <li class="ing" v-for="(ing, index) in smoothie.ingredients" :key="index">{{ ing }}</li>
+                        </ul>
+                  </div>
+            </div>
       </div>
 </template>
 
@@ -9,12 +16,36 @@ export default {
       name:'Index',
       data(){
             return{
-
+                  smoothies: [
+                        {title: 'Ninja Brew', slug: 'ninja-brew', ingredients: ['banans', 'coffee', 'milk'], id: '1'},
+                        {title: 'Benyouuu Woalah', slug: 'benyouu-woalah', ingredients: ['apple', 'juice', 'strawberry'], id: '2'}
+                  ]
             }
       }
 }
 </script>
 
-<style scoped>
-
+<style>
+.index{
+      display: grid;
+      grid-template-columns: 1fr 1fr 1fr;
+      grid-gap: 30px;
+      margin-top: 60px;
+}
+.index h2{
+      font-size: 1.8em;
+      text-align: center;
+      margin-top: 0px;
+}
+.index .ingredients{
+      list-style: none;
+}
+.index .ingredients .ing{
+      border-radius: 10px;
+      background-color: #eee;
+      display: inline-block;
+      margin-right: 10px;
+      padding: 5px 5px;
+      text-transform: capitalize;
+}
 </style>
