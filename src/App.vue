f4b098ade38f653826d84f86ca8f0d1a31da81b6
benyou1969 <ismail.mca191@gmail.com>
2019-05-06 21:01:04 +0100

    Install vue

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,19 @@
 <template>
   <div id="app">
-    <Navbar />
     <img src="./assets/logo.png">
-    <router-view />
+    <router-view/>
   </div>
 </template>
 
 <script>
-import Navbar from './components/Navbar'
 export default {
-  name: "App",
-  components: {
-    Navbar
-  }
-};
+  name: 'App'
+}
 </script>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/router/index.js
+++ src/router/index.js
@@ -1,42 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
-import Home from '@/components/Home'
-import About from '@/components/About'
-import Contact from '@/components/Contact'
-import Profile from '@/components/Profile'
-
+import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
-// Why the symbol # is there between the '/#/' 
-// if we go to a website and type '/home' what's happening? we making a request to the serve and the server looked the request and said okay i'll need to send a home page to the browser...
-// but when we using vuejs in the front-end we don't want to be sending these requests to the serve all the time we need vuejs to take ccare all the work and when it sees the '/#/home' it prevents that request because this does not send request to the serve then vue js looked this url and says  i'll handle this route and i'll send you to home or about component,and that's why the hash symbol is there
-
-//  but it still not awesome to publish that in production mode  so how we remove it
-
 export default new Router({
-      mode : 'history',
-      routes: [
-            {
-                  path: '/',
-                  name: 'Home',
-                  component: Home
-            },
-            {
-                  path: '/about',
-                  name: 'About',
-                  component: About
-            },
-            {
-                  path: '/contact',
-                  name: 'Contact',
-                  component: Contact
-            },
-            {
-                  path: '/users/:user_id',
-                  name: 'Profile',
-                  component: Profile
-            }
-      ]
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }
+  ]
 })
