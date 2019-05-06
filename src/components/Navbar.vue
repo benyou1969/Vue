3ff9178c4304237e1b065b94b0d25b335df8a894
benyou1969 <ismail.mca191@gmail.com>
2019-05-06 22:05:02 +0100

    Navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+      <div class="navbar navbar-fixed">
+            <nav class="nav-extended purple darken-1">
+                 <div class="nav-content">
+                        <router-link to="">
+                              <span class="nav-title">Smoothies</span> 
+                        </router-link>
+                        <a href="" class="btn-large  btn-floating halfway-fab pink">
+                              <router-link to="">
+                                <i class="material-icons">add</i>
+                              </router-link>
+                        </a>
+                 </div>
+            </nav>
+      </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+    data(){
+          return {
+
+          }
+    }  
+}
+</script>
+
+<style>
+.navbar{
+      margin-bottom: 50px;
+}
+ .navbar nav{
+       padding: 0 20px;
+ }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,9 +4,9 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
+  // routes: [
+  //   {
       
-    }
-  ]
+  //   }
+  // ]
 })
