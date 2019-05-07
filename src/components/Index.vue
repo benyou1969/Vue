f56b3988b18755ac73000b135fac3dab648a6395
benyou1969 <ismail.mca191@gmail.com>
2019-05-07 11:46:49 +0100

    deleting (local) data

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -2,6 +2,8 @@
       <div class="index container">
             <div class="card" v-for="smoothie in smoothies" :key="smoothie.id">
                   <div class="card-content">
+                  <i @click="deleteSmoothie(smoothie.id)" class="material-icons delete">delete</i>
+
                         <h2 class="purple-text">{{ smoothie.title }}</h2>
                         <ul class="ingredients">
                               <li class="ing" v-for="(ing, index) in smoothie.ingredients" :key="index">{{ ing }}</li>
@@ -21,6 +23,14 @@ export default {
                         {title: 'Benyouuu Woalah', slug: 'benyouu-woalah', ingredients: ['apple', 'juice', 'strawberry'], id: '2'}
                   ]
             }
+      },
+      methods:{
+           deleteSmoothie(id){
+                 this.smoothies = this.smoothies.filter(smoothie => {
+                       alert('confirm')
+                       return smoothie.id != id
+                 })
+           }
       }
 }
 </script>
@@ -32,15 +42,25 @@ export default {
       grid-gap: 30px;
       margin-top: 60px;
 }
-.index h2{
+.index .card .card-content .delete{
+      position: absolute;
+      top: 4px;
+      right: 4px;
+      cursor: pointer;
+      /* float: right; */
+      color: #aaa;
+      font-size: 1.4em;
+      
+}
+.index .card h2{
       font-size: 1.8em;
       text-align: center;
       margin-top: 0px;
 }
-.index .ingredients{
+.index .card .ingredients{
       list-style: none;
 }
-.index .ingredients .ing{
+.index .card .ingredients .ing{
       border-radius: 10px;
       background-color: #eee;
       display: inline-block;
