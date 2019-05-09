b3015e04078af80d924f7991dc65e166a419ad3f
benyou1969 <ismail.mca191@gmail.com>
2019-05-09 22:24:59 +0100

    firestore queries

--- src/components/EditSmoothie.vue
+++ src/components/EditSmoothie.vue
@@ -1,16 +1,31 @@
 <template>
-       <div class="edit-smoothie container">
-            <h2>Edit a smoothie {{ this.$route.params.smoothie_slug }}</h2>
+       <div class="edit-smoothie container" v-if="smoothie">
+            <h2 >Edit a smoothie {{ smoothie.title }}</h2>
+            <form>
+
+            </form>
       </div>
 </template>
 
 <script>
+import db from '@/firebase/init'
 export default {
       name: 'EditSmoothie',
       data(){
             return{
-                  
+                  smoothie: null,
             }
+      },
+      created(){
+            let ref = db.collection('smoothies').where('slug', '==', this.$route.params.smoothie_slug )
+            ref.get().then(snapshot => {
+                  snapshot.forEach(doc => {
+                        this.smoothie = doc.data()
+                        this.smoothie.id = doc.id
+                        console.log(doc.data())
+                  })
+            })
+           
       }
 }
 </script>
