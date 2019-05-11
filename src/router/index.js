0f4387332f61c01a6601ec50c872487473ef3bb2
benyou1969 <ismail.mca191@gmail.com>
2019-05-12 00:41:49 +0100

    remove files from the last project

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Index from '@/components/Index'
-import AddSmoothie from '@/components/AddSmoothie'
-import EditSmoothie from'@/components/EditSmoothie'
+import HelloWorld from '@/components/HelloWorld'
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-        path: '/',
-        name: 'Index',
-        component: Index
-    },
-    {
-         path: '/addsmoothie',
-         name: 'AddSmoothie',
-         component: AddSmoothie
-    },
-    {
-      path: '/edit-smoothie/:smoothie_slug',
-      name: 'EditSmoothie',
-      component: EditSmoothie
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
     }
   ]
 })
