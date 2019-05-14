da53430a24ac6ccb309fd7a2762a13d77962046f
benyou1969 <ismail.mca191@gmail.com>
2019-05-14 01:56:15 +0100

    create chat window

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,6 +1,20 @@
 <template>
-      <div class="contianer chat">
-            <h2>Chat, {{ this.name }}</h2>
+      <div class="container chat">
+            <h2 class="center-align teal-text">Chat</h2>
+            <div class="card">
+                  <div class="card-content">
+                        <ul class="messages">
+                              <li>
+                                    <span class="teal-text">name</span>
+                                    <span class="grey-text md-darken-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nemo tempore quibusdam dolor voluptates ipsum modi, quisquam nisi nostrum voluptatem veniam, quaerat odio incidunt delectus consequatur porro, quam repellat aliquid?</span>
+                                    <span class="dark-text time">10:20pm</span>
+                              </li>
+                        </ul>
+                  </div>
+                  <div class="card-action">
+                        <input type="text">
+                  </div>
+            </div>
       </div>
 </template>
 
@@ -15,3 +29,16 @@ export default {
       }
 }
 </script>
+
+<style>
+.chat h2{
+      font-size: 2.6em;
+}
+.chat span{
+      font-size: 1.4em;
+}
+.chat .time {
+      display: block;
+      font-size: 1.2em;
+}
+</style>
