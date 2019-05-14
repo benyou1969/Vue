b00034e77a2e394dcdf23119e4b55f5af511fa6e
benyou1969 <ismail.mca191@gmail.com>
2019-05-14 01:33:46 +0100

    Passing props via routes
    Create a chat component

---
+++ src/components/Chat.vue
@@ -0,0 +1,17 @@
+<template>
+      <div class="contianer chat">
+            <h2>Chat, {{ this.name }}</h2>
+      </div>
+</template>
+
+<script>
+export default {
+      name: 'Chat',
+      props: ['name'],
+      data(){
+            return{
+
+            }
+      }
+}
+</script>
