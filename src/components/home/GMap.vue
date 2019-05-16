677b61a63178a870e8720e9ca29cab44dc30f36c
benyou1969 <ismail.mca191@gmail.com>
2019-05-16 20:16:25 +0100

    Map component

---
+++ src/components/home/GMap.vue
@@ -0,0 +1,21 @@
+<template>
+      <div>
+            <h2>Map</h2>
+      </div>
+</template>
+
+<script>
+export default {
+      name:'GMap',
+      data(){
+            return {
+
+            }
+      }
+}
+</script>
+
+
+<style>
+
+</style>
